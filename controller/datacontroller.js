8818f9e3cde150678093dd076818d0e6557bc663
Lounnaci-gi <ahmedlounnaci@gmail.com>
2025-01-21 23:02:38 +0100

    Ajout de find par id_client

--- controller/datacontroller.js
+++ controller/datacontroller.js
@@ -31,11 +31,11 @@ module.exports.editpost = async (req, res) => {
         if (!id) {
             return res.status(400).send("L'Id n'existe pas.");
         }
-     const updatepost = await ClientPost.findByIdAndUpdate(
-        id,
-        req.body,
-        {new:true}
-     )   
+        const updatepost = await ClientPost.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true }
+        )
         res.status(200).send("mise a jour effectuer success.");
     }
     catch (err) {
@@ -43,3 +43,36 @@ module.exports.editpost = async (req, res) => {
     }
 }
 
+
+module.exports.getposts = async (req, res) => {
+    try {
+        if (!res.status == 200) {
+            return res.status(400).send("Une erreur est survenue");
+        }
+        const getpost = await ClientPost.find();
+        //res.status(200).send("Récupération effectuer success.");
+        res.status(200).json(getpost);
+
+    }
+    catch (err) {
+        res.status(500).send("Une erreur est survenue.");
+    }
+}
+
+module.exports.get_with_id_client = async (req, res) => {
+    try {
+        if (!req.params.id == "") {
+            const nombre = await ClientPost.find({ Id_Client: req.params.id });
+            if (nombre.length > 0) {
+                res.status(200).json(nombre);
+            } else {
+                res.send(`Aucun enregistrement Trouvès ne coorespond a l'id : `+req.params.id);
+            }
+        } else {
+            res.send('Veuillez Fournir un Id_Client');
+        }
+    } catch (err) {
+        res.status(400).send(`Impossible d'accéder a l'enregistrement.`);
+
+    }
+}
