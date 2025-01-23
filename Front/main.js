4d6c9ae5c1d6294051d1e616ad4675c510b367f6
Lounnaci-gi <ahmedlounnaci@gmail.com>
2025-01-23 21:59:22 +0100

    Ajout de css (ce n'est qu'un début)

--- Front/main.js
+++ Front/main.js
@@ -8,7 +8,26 @@ btn.addEventListener('click', async () => {
         }
         const posts = await response.json();
         const count = posts.length;
-        alert(`Nombre d'enregistrements : ${count}`);
+
+        const ttable = document.getElementById("table");
+        ttable.innerHTML = `
+          <tr>
+            <th>ID_Client</th>
+            <th>Raison Sociale</th>
+            <th>Adresse</th>
+            <th>Email</th>
+          </tr>`;
+        posts.forEach(e => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                        <td>${e.Id_Client}</td>
+                        <td>${e.raison_sociale}</td>
+                        <td>${e.Adresse}</td>
+                        <td>${e.email}</td>
+                        `;
+            ttable.appendChild(row);
+        });
+
 
     }
     catch (error) {
